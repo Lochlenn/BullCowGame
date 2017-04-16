ed6796d35b6544ccdc36b42b493891f8d95108fd
Lochlenn Loch <feichinger6679@gmail.com>
2017-04-16 18:51:41 -0400

    03 added constant expression and added more text

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@ using namespace std;
 
 int main() 
 {
-	cout << "Welcome to Bulls and Cows\n";
+	constexpr int WORD_LENGTH = 5;
+
+	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	std::cout << "Can you guess the " << WORD_LENGTH;
+	std::cout << " letter isogram that I'm thinking of?\n";
 	return 0;
 }
