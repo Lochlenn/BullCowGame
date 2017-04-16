e8e70f2c2050a0b3302454b70b05d4ef68c50a99
Lochlenn Loch <feichinger6679@gmail.com>
2017-04-16 19:03:18 -0400

    04 Take input, bad coding, double code for example

--- main.cpp
+++ main.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main() 
 {
+	// introduce the game
 	constexpr int WORD_LENGTH = 5;
 
 	std::cout << "Welcome to Bulls and Cows, a fun word game.\n";
 	std::cout << "Can you guess the " << WORD_LENGTH;
 	std::cout << " letter isogram that I'm thinking of?\n";
+
+	// get a guess from the player
+	std::cout << "\n\nEnter a guess: ";
+	std::string Guess = "";
+	std::cin >> Guess;
+
+	// repeat guess back to player
+	std::cout << "\nYour guess was " << Guess << ".\n";
+
+	// get a guess from the player
+	std::cout << "\n\nEnter a guess: ";
+	std::cin >> Guess;
+
+	// repeat guess back to player
+	std::cout << "\nYour guess was " << Guess << ".\n";
 	return 0;
 }
