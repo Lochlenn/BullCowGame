e8104dfd12c5dcd0ee93d0afd58b88c90de8bacc
Lochlenn Loch <feichinger6679@gmail.com>
2017-04-17 19:51:39 -0400

    12, 13- Messed up by reverting and the numbers, should be fixed now

---
+++ FBullCowGame.cpp
@@ -0,0 +1,26 @@
+#include "FBullCowGame.h"
+
+void FBullCowGame::Reset()
+{
+	return;
+}
+
+int FBullCowGame::GetMaxTries()
+{
+	return 0;
+}
+
+bool FBullCowGame::CheckGuessValidity(std::string)
+{
+	return false;
+}
+
+bool FBullCowGame::IsGameWon()
+{
+	return false;
+}
+
+int FBullCowGame::GetCurrentTry()
+{
+	return 0;
+}
