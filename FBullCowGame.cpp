6e4cd673186c013eb4a6cabdcbd40f7a74cef48b
Lochlenn Loch <feichinger6679@gmail.com>
2017-04-18 16:10:05 -0400

    19 added struct to count bulls and cows, added to reset function

--- FBullCowGame.cpp
+++ FBullCowGame.cpp
@@ -5,14 +5,39 @@ using int32 = int;
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 
+FBullCowGame::FBullCowGame()
+{
+
+}
+
 void FBullCowGame::Reset()
 {
+	constexpr int32 MAX_TRIES = 8;
+	MyMaxTries = MAX_TRIES;
+
+	const FString HIDDEN_WORD = "planet";
+	MyHiddenWord = HIDDEN_WORD;
+
+	MyCurrentTry = 1;
 	return;
 }
 
 bool FBullCowGame::CheckGuessValidity(FString)
 {
 	return false;
+}
+
+// recieves a valid guess, increments turn, and returns count
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+	//increment turn number
+	MyCurrentTry++;
+	// setup a return variable
+	BullCowCount BullCowCount;
+	// loop through letters in guess
+		// compare letters against hidden word 
+
+	return BullCowCount;
 }
 
 bool FBullCowGame::IsGameWon() const
--- FBullCowGame.h
+++ FBullCowGame.h
@@ -4,19 +4,28 @@
 using FString = std::string;
 using int32 = int;
 
+struct BullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
+	FBullCowGame(); // constructor
+
 	int32 GetMaxTries() const;
 	bool IsGameWon() const;
 	int32 GetCurrentTry() const;
 
 	void Reset();  // TODO make a more rich return value
 	bool CheckGuessValidity(FString);
-	// SomeNewType to submit valid guess STRING
-	// provide a method for counting bulls and cows and increasing try #
+
+	BullCowCount SubmitGuess(FString);
 
 
 private:
 	int32 MyCurrentTry = 1;
 	int32 MyMaxTries = 5;
+	FString MyHiddenWord;
 };
